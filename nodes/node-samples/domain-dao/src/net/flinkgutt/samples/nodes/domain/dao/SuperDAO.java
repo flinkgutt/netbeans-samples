f4d92afacd25065a3e9274216ff75efdd7da524f
Christian Michelsen <chris@flinkgutt.net>
2013-05-21 16:29:04 +0200

    SuperDAO now runs the SQL create script.

--- nodes/node-samples/domain-dao/src/net/flinkgutt/samples/nodes/domain/dao/SuperDAO.java
+++ nodes/node-samples/domain-dao/src/net/flinkgutt/samples/nodes/domain/dao/SuperDAO.java
@@ -1,6 +1,16 @@
-
 package net.flinkgutt.samples.nodes.domain.dao;
 
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
@@ -10,13 +20,14 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
  */
 public abstract class SuperDAO {
 
-    public static NamedParameterJdbcTemplate jdbcTemplate;
+    public NamedParameterJdbcTemplate jdbcTemplate;
 
     public enum DBServer {
 
         MySQL, PostgreSQL
     }
     public DBServer selected = DBServer.MySQL;
+    private PropertyChangeSupport pcs = new PropertyChangeSupport(this);
 
     public SuperDAO() {
         if (jdbcTemplate == null) {
@@ -34,7 +45,54 @@ public abstract class SuperDAO {
                 dataSource.setPassword("secretpassword123");
             }
             jdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
-
+            try {
+                checkForSampleDB();
+            } catch (IOException ex) {
+                Logger.getLogger(SuperDAO.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
+    }
+
+    private void checkForSampleDB() throws IOException {
+        // Run SQL Create script based on what DB we have chosen to use
+        switch (selected) {
+            case MySQL:
+                // Load mysql.sql
+                jdbcTemplate.getJdbcOperations().batchUpdate(removeEmptyLines("mysql.sql"));
+                break;
+            case PostgreSQL:
+                // Load postgresql.sql
+                break;
+        }
+
+
+    }
+
+    private String[] removeEmptyLines(String file) throws FileNotFoundException {
+        String text = "";
+        try {
+            text = new Scanner(new File(getClass().getResource(file).toURI()), "UTF-8").useDelimiter("\\A").next();
+        } catch (URISyntaxException ex) {
+            Logger.getLogger(SuperDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        String[] tmp = text.split(";");
+        // jdbctemplate.batchUpdate() does _NOT_ like empty sql queries, so we need to remove every empty line we can find.
+        // Yes, this is ugly. I know.
+        List<String> arr = new ArrayList<String>();
+        for (int i = 0; i < tmp.length; i++) {
+            String string = tmp[i];
+            if (!string.trim().isEmpty()) {
+                arr.add(string);
+            }
+        }
+        return arr.toArray(new String[arr.size()]);
+    }
+
+    public void addPropertyChangeListener(PropertyChangeListener pcl) {
+        pcs.addPropertyChangeListener(pcl);
+    }
+
+    public void removePropertyChangelistener(PropertyChangeListener plc) {
+        pcs.removePropertyChangeListener(plc);
     }
 }
--- nodes/node-samples/domain-dao/src/net/flinkgutt/samples/nodes/domain/dao/mysql.sql
+++ nodes/node-samples/domain-dao/src/net/flinkgutt/samples/nodes/domain/dao/mysql.sql
@@ -1,8 +1,5 @@
-# MySQL netbeans-samples create script
-# by Christian Michelsen 21.05.2013
-
-DROP TABLE IF EXISTS `netbeans-samples`.categories;
-CREATE TABLE `netbeans-samples`.categories (
+DROP TABLE IF EXISTS categories;
+CREATE TABLE categories (
 	category_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	parent_id INT,
 	category_name VARCHAR(100),
@@ -13,20 +10,22 @@ ENGINE=InnoDB
 DEFAULT CHARSET=utf8
 COLLATE=utf8_unicode_ci;
 
-INSERT INTO `netbeans-samples`.categories (parent_id,category_name, category_description, active) VALUES(0, "Category #1",null,true);
-INSERT INTO `netbeans-samples`.categories (parent_id,category_name, category_description, active) VALUES(0, "Category #2",null,true);
-INSERT INTO `netbeans-samples`.categories (parent_id,category_name, category_description, active) VALUES(0, "Category #3",null,true);
-INSERT INTO `netbeans-samples`.categories (parent_id,category_name, category_description, active) VALUES(0, "Category #4",null,true);
-INSERT INTO `netbeans-samples`.categories (parent_id,category_name, category_description, active) VALUES(0, "Category #5",null,true);
+# Insert some test data
+INSERT INTO categories (parent_id,category_name, category_description, active) VALUES(0, "Books",null,true);
+INSERT INTO categories (parent_id,category_name, category_description, active) VALUES(0, "Movies",null,true);
+INSERT INTO categories (parent_id,category_name, category_description, active) VALUES(0, "Posters",null,true);
+INSERT INTO categories (parent_id,category_name, category_description, active) VALUES(0, "Online courses",null,true);
+INSERT INTO categories (parent_id,category_name, category_description, active) VALUES(0, "Music",null,true);
 
-INSERT INTO `netbeans-samples`.categories (parent_id,category_name, category_description, active) VALUES(1, "Category #6",null,true);
-INSERT INTO `netbeans-samples`.categories (parent_id,category_name, category_description, active) VALUES(2, "Category #7",null,true);
-INSERT INTO `netbeans-samples`.categories (parent_id,category_name, category_description, active) VALUES(2, "Category #8",null,true);
-INSERT INTO `netbeans-samples`.categories (parent_id,category_name, category_description, active) VALUES(2, "Category #9",null,true);
-INSERT INTO `netbeans-samples`.categories (parent_id,category_name, category_description, active) VALUES(3, "Category #10",null,true);
-INSERT INTO `netbeans-samples`.categories (parent_id,category_name, category_description, active) VALUES(3, "Category #11",null,true);
-INSERT INTO `netbeans-samples`.categories (parent_id,category_name, category_description, active) VALUES(3, "Category #12",null,true);
-INSERT INTO `netbeans-samples`.categories (parent_id,category_name, category_description, active) VALUES(5, "Category #13",null,true);
-INSERT INTO `netbeans-samples`.categories (parent_id,category_name, category_description, active) VALUES(5, "Category #14",null,true);
-INSERT INTO `netbeans-samples`.categories (parent_id,category_name, category_description, active) VALUES(5, "Category #15",null,true);
-INSERT INTO `netbeans-samples`.categories (parent_id,category_name, category_description, active) VALUES(5, "Category #16",null,true);
+INSERT INTO categories (parent_id,category_name, category_description, active) VALUES(1, "Crime",null,true);
+INSERT INTO categories (parent_id,category_name, category_description, active) VALUES(2, "Action",null,true);
+INSERT INTO categories (parent_id,category_name, category_description, active) VALUES(2, "Comedy",null,true);
+INSERT INTO categories (parent_id,category_name, category_description, active) VALUES(2, "Drama",null,true);
+INSERT INTO categories (parent_id,category_name, category_description, active) VALUES(3, "Thriller",null,true);
+INSERT INTO categories (parent_id,category_name, category_description, active) VALUES(3, "Romance",null,true);
+INSERT INTO categories (parent_id,category_name, category_description, active) VALUES(3, "Bollywood",null,true);
+
+INSERT INTO categories (parent_id,category_name, category_description, active) VALUES(5, "Jazz",null,true);
+INSERT INTO categories (parent_id,category_name, category_description, active) VALUES(5, "Disco",null,true);
+INSERT INTO categories (parent_id,category_name, category_description, active) VALUES(5, "Rock",null,true);
+INSERT INTO categories (parent_id,category_name, category_description, active) VALUES(5, "Techno",null,true);
